dec655b63121339279063732fd2d15ea385b3a35
Daniel Collins <danielc111@gmail.com>
2020-12-08 22:35:02 -0500

    Add simple integration test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,11 +5,57 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+const testFileContent = 
+`export class TestClass {
+  name?: string;
+
+
+}
+`;
+
+suite('Extension Test Suite', function () {
+	this.timeout(5000);
+
+	let textContent = '';
+	suiteSetup(async () => {
+		vscode.window.showInformationMessage('Start all tests.');
+
+		try {
+			const openedTextDocument = await vscode.workspace.openTextDocument({
+				language: 'typescript',
+				content: testFileContent,
+			});
+			console.info('opened document', openedTextDocument);
+			
+			const editorWindow = await vscode.window.showTextDocument(openedTextDocument);
+			editorWindow.options.insertSpaces = true;
+			editorWindow.options.tabSize = 2;
+			
+			editorWindow.selection = new vscode.Selection(3, 3, 3, 3);
+
+			await vscode.commands.executeCommand('tsCopyConstructor.insert');
+
+			textContent = openedTextDocument.getText();		
+			console.info(textContent);
+		} catch (e) {
+			console.info('some error happened', e);
+		}
+	});
 
 	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+		assert.notStrictEqual(textContent, testFileContent);
+
+		const expectedTextContent = 
+		`export class TestClass {
+  name?: string;
+
+  constructor(opts?: Partial<TestClass>) {
+    if (opts?.name != null) {
+      this.name = opts.name;
+    }
+  }
+}
+`;
+		assert.strictEqual(textContent, expectedTextContent);
 	});
 });
