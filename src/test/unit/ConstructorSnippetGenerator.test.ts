50afe56e9b00e3ec62098d70407caea2a896c5a1
Daniel Collins <danielc111@gmail.com>
2021-01-07 21:24:16 -0500

    Add object instantiation tests

--- src/test/unit/ConstructorSnippetGenerator.test.ts
+++ src/test/unit/ConstructorSnippetGenerator.test.ts
@@ -8,7 +8,7 @@ const parser = new TypescriptParser();
 const twoSpacesIndentationSpec = new IndentationSpec(true, 2);
 
 describe('ConstructorSnipperGenerator.ts', async function() {
-  it('should generate constructor for class with a single string property', async function() {
+  it('should generate constructor with a single string property', async function() {
 
     const sampleClass = 
 `export class TestClass {
@@ -33,7 +33,7 @@ describe('ConstructorSnipperGenerator.ts', async function() {
     strictEqual(expectedOutput, generatedSnippet);
   });
 
-  it('should generate constructor for class with a single number property', async function() {
+  it('should generate constructor with a single number property', async function() {
 
     const sampleClass = 
 `export class TestClass {
@@ -77,6 +77,86 @@ describe('ConstructorSnipperGenerator.ts', async function() {
 `  constructor(opts?: Partial<TestClass>) {
     if (opts?.string != null) {
       this.string = [...opts.string];
+    }
+  }`;
+    
+    strictEqual(expectedOutput, generatedSnippet);
+  });
+
+  it('should generate constructor and instantiate object of known type', async function() {
+
+    const sampleClass = 
+`export class TestClass {
+  testObject: TestObject;
+}`;
+
+    const testObjectClass = 
+`export class TestObject {
+  constructor(opts?: Partial<TestObject>) {
+
+  }
+}`;
+    
+    const activeDocumentParseResult = await parser.parseSource(sampleClass);
+
+    const declarations = new Declarations(activeDocumentParseResult);
+
+    const testObjectClassDeclaration = await parser.parseSource(testObjectClass);
+
+    testObjectClassDeclaration.declarations.forEach(declaration => {
+      if (declaration.name) {
+          declarations.importsDeclarationMap.set(declaration.name, declaration);
+      }
+    });
+
+    const snippetGenerator = new ConstructorSnippetGenerator(declarations, twoSpacesIndentationSpec);
+
+    const generatedSnippet = snippetGenerator.generateSnippet();
+
+    const expectedOutput = 
+`  constructor(opts?: Partial<TestClass>) {
+    if (opts?.testObject != null) {
+      this.testObject = new TestObject(opts.testObject);
+    }
+  }`;
+    
+    strictEqual(expectedOutput, generatedSnippet);
+  });
+
+  it('should generate constructor and instantiate an array of objects of known type', async function() {
+
+    const sampleClass = 
+`export class TestClass {
+  testObjects: TestObject[] = [];
+}`;
+
+    const testObjectClass = 
+`export class TestObject {
+  constructor(opts?: Partial<TestObject>) {
+
+  }
+}`;
+    
+    const activeDocumentParseResult = await parser.parseSource(sampleClass);
+
+    const declarations = new Declarations(activeDocumentParseResult);
+
+    const testObjectClassDeclaration = await parser.parseSource(testObjectClass);
+
+    testObjectClassDeclaration.declarations.forEach(declaration => {
+      if (declaration.name) {
+          declarations.importsDeclarationMap.set(declaration.name, declaration);
+      }
+    });
+
+    const snippetGenerator = new ConstructorSnippetGenerator(declarations, twoSpacesIndentationSpec);
+
+    const generatedSnippet = snippetGenerator.generateSnippet();
+
+    const expectedOutput = 
+`  constructor(opts?: Partial<TestClass>) {
+    if (opts?.testObjects != null) {
+      this.testObjects = opts.testObjects.map(val => new TestObject(val));
     }
   }`;
     
